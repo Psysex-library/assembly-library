9a224de90d9fe30b137ff7a6cee075fe27487773
Cayden <caydenlincke4@gmail.com>
2024-10-08 11:48:59 +0200

    boot.asm aktualisieren

--- assembley/PysexOS/boot.asm
+++ assembley/PysexOS/boot.asm
@@ -1,38 +1,52 @@
 [BITS 16]          
-
 org 0x7c00         
 
 start:
     mov ah, 0x0E   
-    mov al, 'H'    
+    mov al, 'L'    
     int 0x10       
+    mov al, 'o'
+    int 0x10
+    mov al, 'a'
+    int 0x10
+    mov al, 'd'
+    int 0x10
+    mov al, 'i'
+    int 0x10
+    mov al, 'n'
+    int 0x10
+    mov al, 'g'
+    int 0x10
+    mov al, ' '
+    int 0x10
+    mov al, 'K'
+    int 0x10
+    mov al, 'e'
+    int 0x10
+    mov al, 'r'
+    int 0x10
+    mov al, 'n'
+    int 0x10
     mov al, 'e'
     int 0x10
     mov al, 'l'
     int 0x10
-    mov al, 'l'
-    int 0x10
-    mov al, 'o'
-    int 0x10
-    mov al, ','
-    int 0x10
-    mov al, ' '
-    int 0x10
-    mov al, 'W'
-    int 0x10
-    mov al, 'o'
-    int 0x10
-    mov al, 'r'
-    int 0x10
-    mov al, 'l'
-    int 0x10
-    mov al, 'd'
-    int 0x10
     mov al, '!'
     int 0x10
 
+    mov bx, 0x1000             
+    mov dh, 1                  
+    mov dl, 0x80               
+    mov ah, 0x02               
+    mov al, 1                  
+    mov ch, 0                  
+    mov cl, 2                  
+    int 0x13                   
+
+    jmp 0x1000                 
+
 hang:
-    jmp hang       
+    jmp hang                   
 
 times 510-($-$$) db 0  
-dw 0xAA55             
+dw 0xAA55              
